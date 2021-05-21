4f550081bb2d233e46ebaa7b8f2d0024a039ed64
Pawel <zalewskipawel1998@gmail.com>
2021-05-21 12:26:46 +0200

    Hello pawel

---
+++ src/Bicycle.java
@@ -0,0 +1,19 @@
+public class Bicycle
+{
+    int speed = 0;
+    int gear = 0;
+    int wheels = 1;
+
+    void changeSpeed(int value)
+    {
+        speed = value;
+    }
+    void changeWheelsAmount(int value)
+    {
+        wheels = value;
+    }
+    void changeGear(int value)
+    {
+        gear = value;
+    }
+}
